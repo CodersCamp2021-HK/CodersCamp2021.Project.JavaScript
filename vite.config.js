ddaf7333fc3bda7c840e07a54d23ddecfb2154ee
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2021-11-27 14:16:43 +0100

    tools: add eslint, prettier and tsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-default-export */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        fs: {
-            strict: true,
-        },
+  server: {
+    fs: {
+      strict: true,
     },
+  },
 });
