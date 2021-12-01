a7b1a976588d29de0a6b91a4c624c8842196b31a
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2021-12-01 23:45:10 +0100

    chore(deps): remove unnecessary babel deps

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ const config = {
     '^.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
   },
   transform: {
-    '^.+\\.js$': 'babel-jest',
     '^.+\\.(png|jpg|jpeg|gif|webp|mp4|mp3|svg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
