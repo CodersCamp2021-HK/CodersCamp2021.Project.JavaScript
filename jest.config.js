96fe6d8967e8437a4571ce283fe89a561b55c36c
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2021-12-02 15:11:48 +0100

    fix: jest transform of node_modules

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ const config = {
     '^.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
   },
   transform: {
+    '^.+\\.js$': 'babel-jest',
     '^.+\\.(png|jpg|jpeg|gif|webp|mp4|mp3|svg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
