9003162f3ca247054b8143a1cd6698d39d34a294
tchojnacki <tomaszchojnacki2001@gmail.com>
2021-12-07 18:19:14 +0100

    test(data): remove unused keys from mock responses

--- src/data/api.test.js
+++ src/data/api.test.js
@@ -18,18 +18,7 @@ const fetchMock = async (url) => {
           info: { count: 1, pages: 1, next: null, prev: null },
           results: [
             {
-              id: 1,
               name: 'Rick Sanchez',
-              status: 'Alive',
-              species: 'Human',
-              type: '',
-              gender: 'Male',
-              origin: { name: 'Earth (C-137)', url: 'https://rickandmortyapi.com/api/location/1' },
-              location: { name: 'Citadel of Ricks', url: 'https://rickandmortyapi.com/api/location/3' },
-              image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-              episode: ['https://rickandmortyapi.com/api/episode/1'],
-              url: 'https://rickandmortyapi.com/api/character/1',
-              created: '2017-11-04T18:48:46.250Z',
             },
           ],
         }),
@@ -43,13 +32,7 @@ const fetchMock = async (url) => {
           info: { count: 1, pages: 1, next: null, prev: null },
           results: [
             {
-              id: 1,
               name: 'Earth (C-137)',
-              type: 'Planet',
-              dimension: 'Dimension C-137',
-              residents: ['https://rickandmortyapi.com/api/character/38'],
-              url: 'https://rickandmortyapi.com/api/location/1',
-              created: '2017-11-10T12:42:04.162Z',
             },
           ],
         }),
@@ -67,18 +50,7 @@ const fetchMock = async (url) => {
         ok: true,
         status: 200,
         json: async () => ({
-          id: 20,
           name: 'Ants in my Eyes Johnson',
-          status: 'unknown',
-          species: 'Human',
-          type: 'Human with ants in his eyes',
-          gender: 'Male',
-          origin: { name: 'unknown', url: '' },
-          location: { name: 'Interdimensional Cable', url: 'https://rickandmortyapi.com/api/location/6' },
-          image: 'https://rickandmortyapi.com/api/character/avatar/20.jpeg',
-          episode: ['https://rickandmortyapi.com/api/episode/8'],
-          url: 'https://rickandmortyapi.com/api/character/20',
-          created: '2017-11-04T22:34:53.659Z',
         }),
       };
     }
@@ -87,13 +59,7 @@ const fetchMock = async (url) => {
         ok: true,
         status: 200,
         json: async () => ({
-          id: 5,
           name: 'Anatomy Park',
-          type: 'Microverse',
-          dimension: 'Dimension C-137',
-          residents: ['https://rickandmortyapi.com/api/character/12'],
-          url: 'https://rickandmortyapi.com/api/location/5',
-          created: '2017-11-10T13:08:46.060Z',
         }),
       };
     }
