670a85069f19368a86117e9726367b6b84d6a86c
Dudek Kamil <dudekkam@codelab.eu>
2021-12-08 21:22:37 +0100

    test: delete unnecessary testcases

--- src/shared/ranking.test.js
+++ src/shared/ranking.test.js
@@ -1,41 +1,17 @@
-/**
- * @jest-environment jsdom
- */
 import { addUserToRanking, getRankingByCategory } from './ranking';
 
-test("add user to localStorage when category easy doesn't exist", () => {
-  expect(addUserToRanking('Tom', 21, 'easy')).toStrictEqual({
-    category: 'easy',
+test('add first user to localStorage, new category', () => {
+  expect(addUserToRanking('Tom', 21, 'character')).toStrictEqual({
+    category: 'character',
     userData: [{ points: 21, username: 'Tom' }],
   });
 });
 
-test("add user to localStorage when category hard doesn't exist", () => {
-  expect(addUserToRanking('Steve', 14, 'hard')).toStrictEqual({
-    category: 'hard',
-    userData: [{ points: 14, username: 'Steve' }],
-  });
-});
-
-test('add user to localStorage, category hard', () => {
-  expect(addUserToRanking('John', 28, 'hard')).toStrictEqual({
-    category: 'hard',
+test('add user to localStorage, category character', () => {
+  expect(addUserToRanking('John', 28, 'character')).toStrictEqual({
+    category: 'character',
     userData: [
-      { points: 14, username: 'Steve' },
-      { points: 28, username: 'John' },
-    ],
-  });
-});
-
-test('get users from localStorage, category easy', () => {
-  expect(getRankingByCategory('easy')).toStrictEqual({ category: 'easy', userData: [{ username: 'Tom', points: 21 }] });
-});
-
-test('get users from localStorage, category hard', () => {
-  expect(getRankingByCategory('hard')).toStrictEqual({
-    category: 'hard',
-    userData: [
-      { points: 14, username: 'Steve' },
+      { points: 21, username: 'Tom' },
       { points: 28, username: 'John' },
     ],
   });
