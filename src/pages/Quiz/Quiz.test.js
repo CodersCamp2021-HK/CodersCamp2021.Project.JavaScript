579e17a2734af25f5362b53ee0f4d420613dfbe2
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2021-12-15 15:07:18 +0100

    Strona "Loading" #25  (#39)
    * feat(pages): create a blank loading page
    
    * feat(pages): loading page's structure and styling
    
    * feat(pages): unify background among pages
    
    * fix(pages): update loading screen design
    
    * fix(public): adjust background property
    
    * feat(pages): go to quiz page after question gen
    
    * test(pages): add tests for the loading page
    
    * refactor(pages): use objects instead of switches
    
    * feat(pages): add minumum loading screen duration
    
    * test(pages): fix page navigation test

---
+++ src/pages/Quiz/Quiz.test.js
@@ -0,0 +1,10 @@
+import { Quiz } from './Quiz';
+
+describe('Quiz page tests', () => {
+  it('Shows quiz page', () => {
+    // @ts-ignore
+    const quiz = Quiz();
+
+    expect(quiz).toBeEmptyDOMElement();
+  });
+});
