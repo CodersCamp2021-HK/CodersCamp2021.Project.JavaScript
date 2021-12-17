320844485adcbdea63cabc013fd617121bbf8605
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2021-12-17 18:16:42 +0100

    add main container

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,11 +1,24 @@
 import { html } from '../../shared';
+import { Button } from '../../components';
+import styles from './Quiz.module.css';
 
 /**
  * @param { { generator: import('../Loading').QuestionGenerator } & import('..').RouterProps } props
  */
 // eslint-disable-next-line no-unused-vars
-function Quiz(props) {
-  return html`<div></div>`;
+function Quiz(router, variant, props) {
+  return html`<div class="${styles.wrapper}">
+    <div class="${styles.quizContainer}">
+      ${Button({
+        onClick: (e) => {
+          e.preventDefault();
+          // router.goto({ page: 'todo', data: { id: x } });
+        },
+        text: `dalej`,
+        variant: 'nextQuestion',
+      })},
+    </div>
+  </div>`;
 }
 
 export { Quiz };
