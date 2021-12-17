c8089e20376de60258434ec241f614768de1c90c
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2021-12-18 00:01:41 +0100

    add new component for all decoration in the background and style quiz container

---
+++ src/components/BackgroundDecoration/BackgroundDecoration.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import { html } from '../../shared';
+import styles from './BackgroundDecoration.module.css';
+
+/**
+ * @returns {HTMLElement}
+ */
+function BackgroundDecoration() {
+  const url = '../src/public/img/ellipse.svg';
+
+  return html`<div>
+    <div class="${styles.bgDecoration1}">
+      <img src="${url}" alt="" />
+    </div>
+    <div class="${styles.bgDecoration2}">
+      <img src="${url}" alt="" />
+    </div>
+    <div class="${styles.bgDecoration3}">
+      <img src="${url}" alt="" />
+    </div>
+    <div class="${styles.bgDecoration4}">
+      <img src="${url}" alt="" />
+    </div>
+  </div>`;
+}
+
+export { BackgroundDecoration };
--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,22 +1,27 @@
 import { html } from '../../shared';
-import { Button } from '../../components';
+import { Button, Logo, BackgroundDecoration } from '../../components';
 import styles from './Quiz.module.css';
 
 /**
  * @param { { generator: import('../Loading').QuestionGenerator } & import('..').RouterProps } props
  */
 // eslint-disable-next-line no-unused-vars
-function Quiz(router, variant, props) {
+function Quiz(props) {
   return html`<div class="${styles.wrapper}">
+  ${BackgroundDecoration()}
     <div class="${styles.quizContainer}">
+      <div class="${styles.headImgWrapper}"">
+        <img src="../src/public/img/RicksHead.png" alt="Ricks head" />
+      </div>
+      <!-- TODO: render here question list component -->
       ${Button({
         onClick: (e) => {
           e.preventDefault();
-          // router.goto({ page: 'todo', data: { id: x } });
         },
         text: `dalej`,
         variant: 'nextQuestion',
-      })},
+      })}
+      <div class="${styles.logoWrapper}">${Logo(31)}</div>
     </div>
   </div>`;
 }
