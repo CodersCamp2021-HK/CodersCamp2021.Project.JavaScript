98ad69a8437bb05bde7a6b3668b82f21c5432513
Dudek Kamil <dudekkam@codelab.eu>
2021-12-18 15:26:16 +0100

    feat(pages): create Home page with buttons and box

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,72 @@
-import { Button } from '../../components';
+import { Button, GameTypes, RulesTextBox } from '../../components';
 import { html } from '../../shared';
+import styles from './Home.module.css';
 
 /**
  * @param {{} & import('..').RouterProps} props
  * @returns
  */
 function Home({ router }) {
-  const pages = [1, 2, 3, 4, 5];
-  return html`<div class="vstack">
-    <h2>Home</h2>
-    ${pages.map((x) =>
-      Button({
-        onClick: (e) => {
-          e.preventDefault();
-          router.goto({ page: 'todo', data: { id: x } });
+  let selectedCategory;
+  let selectedDifficulty;
+
+  return html`<div class="${styles.wrapper}">
+    <section>
+      <div>
+        ${RulesTextBox(
+          'Opis zasad',
+          'What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing andWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and',
+        )};
+      </div>
+      <div>
+      <div>
+        ${GameTypes({
+          onSelect: (selected) => {
+            selectedCategory = selected.id;
+            console.log(selectedCategory);
+          },
+          heading: 'Wybierz kategorię',
+          categories: [
+            { id: 'character', text: 'Co to za postać' },
+            { id: 'episode', text: 'Bohaterowie odcinka' },
+            { id: 'location', text: 'Kto tu mieszka' },
+            { id: 'mixed', text: 'Mieszane' },
+          ],
+        })}
+      </div>
+
+      <div>
+        ${GameTypes({
+          onSelect: (selected) => {
+            selectedDifficulty = selected.id;
+            console.log(selectedDifficulty);
+          },
+          heading: 'Wybierz poziom',
+          categories: [
+            { id: 'easy', text: 'Łatwy' },
+            { id: 'hard', text: 'Trudny' },
+          ],
+          layout: 'halfWidth',
+        })}
+      </div>
+      <div class = ${styles.sth}>
+      ${Button({
+        text: 'Rozpocznij quiz',
+        onClick: () => {
+          console.log(selectedDifficulty);
+          console.log(selectedCategory);
+
+          router.goto({ page: 'loading', data: { selectedCategory, selectedDifficulty } });
         },
-        text: `Go to page ${x}`,
-        variant: 'normal',
-      }),
-    )}
-  </div>`;
+      })}
+      ${Button({
+        text: 'Ranking',
+        onClick: () => {},
+        variant: 'outlined',
+      })}
+      </div>
+    </section>
+  </div></div>`;
 }
 
 export { Home };
