321647eb733047ca346795a450cc6c2a5c083e5f
Dudek Kamil <dudekkam@codelab.eu>
2021-12-19 17:23:16 +0100

    refactor: add box show into updateRulesTextBox

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -25,6 +25,7 @@ function Home({ router }) {
   const updateButtonDisabledState = () => {
     const shouldBeDisabled = selectedCategory === null || selectedDifficulty === null;
     btnStart.disabled = shouldBeDisabled;
+    document.getElementById('rules').className = styles.show;
   };
 
   const updateRulexTextBox = (header, rules) => {
@@ -69,7 +70,6 @@ function Home({ router }) {
                 'Poziom mieszany. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
               );
             }
-            document.getElementById('rules').className = styles.show;
           },
           heading: 'Wybierz kategorię',
           categories: [
@@ -97,7 +97,6 @@ function Home({ router }) {
                 'Poziom trudny. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
               );
             }
-            document.getElementById('rules').className = styles.show;
           },
           heading: 'Wybierz poziom',
           categories: [
