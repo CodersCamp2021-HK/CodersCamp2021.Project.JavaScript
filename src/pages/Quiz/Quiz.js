49d254128d919e1e4058c0f6882bee745cd1207f
mmejer <44590421+mmejer@users.noreply.github.com>
2021-12-21 21:06:31 +0100

    add close and counter element

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,5 +1,5 @@
 import { html } from '../../shared';
-import { Button, Logo, BackgroundDecoration, Question } from '../../components';
+import { Button, Logo, BackgroundDecoration, Question, PopupClose, QuestionCounter } from '../../components';
 import styles from './Quiz.module.css';
 import head from '../../public/img/RicksHead.png';
 
@@ -8,21 +8,28 @@ import head from '../../public/img/RicksHead.png';
  */
 // eslint-disable-next-line no-unused-vars
 function Quiz(props) {
+  const { element: counterElement, increment, getCount } = QuestionCounter();
+  const onClick = () => {
+    const answeredQuestions = getCount();
+    console.log(`Quiz finished after answering ${answeredQuestions} questions!`);
+  };
   // @ts-ignore
   let question = Question(props.generator.next().value);
   return html`<div class="${styles.wrapper}">
   ${BackgroundDecoration()}
     <div class="${styles.quizContainer}">
+      <div class="${styles.counterElementWrapper}">${counterElement}</div>
+      <div class="${styles.popupCloseWrapper}">${PopupClose({ onClick })}</div>
       <div class="${styles.headImgWrapper}"">
         <img src="${head}" alt="Ricks head" />
       </div>
-      
       ${question.question}
       ${Button({
         onClick: (e) => {
           e.preventDefault();
           // @ts-ignore
           question = Question(props.generator.next().value);
+          increment();
           document.getElementById('question').replaceWith(question.question);
         },
         text: 'dalej',
