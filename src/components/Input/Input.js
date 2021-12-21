2739e32139efc9c2a5b84627685e54142b96f821
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2021-12-21 20:01:25 +0100

    add component for input for username

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,12 +2,17 @@ import { html } from '../../shared';
 import styles from './Input.module.css';
 
 /**
- * @param {} props
- * @returns {HTMLButtonElement}
+ * @param {{minCharacters: number, maxCharacters: number, onChange: (e) => void }} props
  */
-function Input({ onChange, minCharacters }) {
-  const input = html`<input placeholder="Nazwa gracza" type="text" />`;
-
+function Input({ minCharacters, maxCharacters = 20, onChange }) {
+  const input = html`<input
+      class="${styles.input}"
+      type="text"
+      minlength="${minCharacters || 1}"
+      maxlength="${maxCharacters}"
+      placeholder="Nazwa gracza"
+    />
+    required`;
   input.addEventListener('input', onChange);
   return input;
 }
