45b4d59449be1d0facc47eddd8b809b56242147b
Justyna S <justyna.skrajna@aliso.pl>
2021-12-27 22:06:30 +0100

    add timer component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,45 +2,89 @@ import { html } from '../../shared';
 import styles from './Timer.module.css';
 
 /**
- * @param {{ ms: number, onElapsed: () => void }} props
+ * @param {{ startingMinutes: number, onFinish: () => void }} props
  * @returns {HTMLElement}
  */
 
-const startingMinutes = 1;
-let time = startingMinutes * 60;
+function Timer({ startingMinutes, onFinish }) {
 
-setInterval(Timer, 1000)
+  let count = startingMinutes * 60,
+    counting;
 
-function Timer() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    let countdown = '';
+  const counter = html`<span class="${styles.counter}">00:00</span>`;
 
-    const displayTimer = html`<div class="${styles.container}">
-    <p>pozostały</p>
-    <svg width="225" height="225" fill="none" viewBox="0 0 240 240" xmlns="http://www.w3.org/2000/svg">
-        <circle class="ellipse5" cx="100" cy="100" r="100" />
-        <circle class="ellipse6" cx="100" cy="100" r="100" />
-        <text id="${styles.counter}" x="50%" y="50%" text-anchor="middle" dy=".4em">${countdown}</text>
-    </svg>
-    <p>czas</p>
-    </div>`
-  
-    if (seconds <10) {
-      countdown = `${minutes}:0${seconds}`
+  let formatDuration = (time) => {
+    let timeObj = new Date(time * 1000);
+
+    function checkIfLessThan10(timeUnit) {
+      return timeUnit < 10 ? `0${timeUnit}` : timeUnit;
     }
-  
-    if (minutes === 0 && String(seconds) === '00') {
-        return countdown = '0:00';
-    } else {
-        countdown = `${minutes}:${seconds}`;
-    };
-    
-    time--;
 
-    // @ts-ignore
-    return displayTimer;
+    let mm = checkIfLessThan10(timeObj.getUTCMinutes());
+    let ss = checkIfLessThan10(timeObj.getUTCSeconds());
+    return `${mm}:${ss}`;
   }
+
+  counter.textContent = formatDuration(count);
+
+  let changeDuration = () => {
+    count -= 1;
+    counter.textContent = formatDuration(count);
+    if (count == 0) {
+      finishCounting();
+      onFinish();
+    }
+  }
+
+  let processCounting = () => {
+    counting = setInterval(changeDuration, 1000);
+  }
+
+  let finishCounting = () => {
+    clearInterval(counting);
+  }
+
+  processCounting();
+
+  const svg = html`
+  <div class="${styles.timerOuterWrapper}">
+  ${counter}
+    <div class="${styles.timerInnerWrapper}">
+      <svg width="200" height="200" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="98" cy="98" r="86.5" stroke="#B2FF59" stroke-width="15"/>
+        <g filter="url(#a)">
+          <circle class="${styles.circle}" cx="98" cy="98" r="86.5" stroke="#fff" stroke-width="15" shape-rendering="crispEdges"/>
+        </g>
+        <defs>
+          <filter id="a" x="0" y="0" width="200" height="200" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="4"/><feGaussianBlur stdDeviation="2"/><feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_65_277"/>
+            <feBlend in="SourceGraphic" in2="effect1_dropShadow_65_277" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  </div>`;
+
+  const displayTimer = html`<div class="${styles.container}">
+    <p class="${styles.text}">pozostały</p>
+      ${svg}
+    <p class="${styles.text}">czas</p>
+  </div>`
+
+
+  const passAnimationDuration = () => {
+    const root = document.querySelector(':root');
+    root.style.setProperty('--animation-time', `${count}s`);
+  }
+
+  passAnimationDuration();
+
+  return displayTimer;
+}
 
 
 export { Timer };
