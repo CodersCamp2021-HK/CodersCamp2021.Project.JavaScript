6adf7dd8534bca173db97c1a10a3d7d522215157
Dudek Kamil <dudekkam@codelab.eu>
2021-12-28 21:22:31 +0100

    feat(Ranking): add Ranking folder to pages

---
+++ src/pages/Ranking/Ranking.js
@@ -0,0 +1,12 @@
+import { html, render } from '../../shared';
+import styles from './Ranking.module.css';
+
+/**
+ * @param {{} & import('..').RouterProps} props
+ * @returns
+ */
+function Ranking({ router }) {
+  return html`<div>Ranking</div>`;
+}
+
+export { Ranking };
