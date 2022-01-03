3e2c3af04b0c205aad7017fddefec91a0c5ee703
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-01-03 20:28:52 +0100

    style(components): implement suggested changes

--- src/components/ContentWrapper/ContentWrapper.js
+++ src/components/ContentWrapper/ContentWrapper.js
@@ -4,16 +4,20 @@ import logoImg from '../../public/img/RickAndMortyLogo.png';
 import { BackgroundDecoration } from '../BackgroundDecoration';
 import styles from './ContentWrapper.module.css';
 
+const head = html`<div class="${styles.headWrapper}"><img src="${headImg}" alt="Głowa Ricka." /></div>`;
+
+const logo = html`<div class="${styles.logoWrapper}"><img src="${logoImg}" alt="Logo serialu." /></div>`;
+
 /**
  *
  * @param {{
  *  hasHead?: boolean,
  *  hasLogo?: boolean,
  *  hasBgDecoration?: boolean,
- *  topLeft?: string | HTMLElement,
- *  topRight?: string | HTMLElement,
- *  botRight?: string | HTMLElement,
- *  topAttachment?: string | HTMLElement,
+ *  topLeft?: HTMLElement,
+ *  topRight?: HTMLElement,
+ *  botRight?: HTMLElement,
+ *  topAttachment?: HTMLElement,
  *  content: string | HTMLElement
  * }} props
  * @returns {HTMLDivElement}
@@ -22,19 +26,14 @@ function ContentWrapper({
   hasHead = false,
   hasLogo = false,
   hasBgDecoration = false,
-  topLeft = '',
-  topRight = '',
-  botRight = '',
-  topAttachment = '',
+  topLeft,
+  topRight,
+  botRight,
+  topAttachment,
   content,
 }) {
-  const head = html`<div class="${styles.headWrapper}"><img src="${headImg}" alt="Głowa Ricka." /></div>`;
-  const logo = html`<div class="${styles.logoWrapper}"><img src="${logoImg}" alt="Logo serialu." /></div>`;
-
-  const hasAttachment = topAttachment !== '';
-
-  const mTop = hasHead || topLeft !== '' || topRight !== '' || hasAttachment;
-  const mBot = hasLogo || botRight !== '';
+  const mTop = hasHead || topLeft || topRight || topAttachment;
+  const mBot = hasLogo || botRight;
 
   const margins = `
   margin-top: ${mTop ? 'var(--panel-padding)' : '0'};
@@ -44,14 +43,12 @@ function ContentWrapper({
   return /** @type {HTMLDivElement} */ (
     html`<div class="${styles.wrapper}">
       ${hasBgDecoration ? BackgroundDecoration() : ''}
-      <section class="${hasAttachment ? styles.panelWithAttachment : styles.panel}" style="${margins}">
-        <div class="${styles.topLeft}">${topLeft}</div>
-        ${hasHead ? head : ''}
-        <div class="${styles.topRight}">${topRight}</div>
-        ${hasAttachment ? html`<div class="${styles.topAttachment}">${topAttachment}</div>` : ''}
+      <section class="${topAttachment ? styles.panelWithAttachment : styles.panel}" style="${margins}">
+        ${topLeft ? html`<div class="${styles.topLeft}">${topLeft}</div>` : ''} ${hasHead ? head : ''}
+        ${topRight ? html`<div class="${styles.topRight}">${topRight}</div>` : ''}
+        ${topAttachment ? html`<div class="${styles.topAttachment}">${topAttachment}</div>` : ''}
         <main class="${styles.content}">${content}</main>
-        ${hasLogo ? logo : ''}
-        <div class="${styles.botRight}">${botRight}</div>
+        ${hasLogo ? logo : ''} ${botRight ? html`<div class="${styles.botRight}">${botRight}</div>` : ''}
       </section>
     </div>`
   );
